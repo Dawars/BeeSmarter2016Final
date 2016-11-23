fa8ad1f0f338f8ca32665858c171f6859dc36d68
dawars <dawars00@gmail.com>
2016-11-23 22:28:07 +0100

    Initial commit

---
+++ app/src/main/java/team/instance/beesmarter2016final/MainActivity.java
@@ -0,0 +1,13 @@
+package team.instance.beesmarter2016final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//stuff
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
